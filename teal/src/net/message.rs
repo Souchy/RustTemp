592f932194584aece14d688af996c821d3bbd282
Souchy <robyn.g@hotmail.fr>
2024-04-14 17:25:13 -0400

    temp - broadcast

--- teal/src/net/message.rs
+++ teal/src/net/message.rs
@@ -1,11 +1,14 @@
-
+use crate::net::client::Client;
 use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
 use std::{
-    any::{self, type_name, Any}, collections::HashMap, error::Error, ptr::null, str::Bytes
+    any::{self, type_name, Any},
+    collections::HashMap,
+    error::Error,
+    ptr::null,
+    str::Bytes,
+    sync::Arc,
 };
-use crate::net::client::Client;
-
 
 #[async_trait]
 pub trait MessageScript {
@@ -22,3 +25,79 @@ pub trait MessageScript {
         client.send_bytes(&buf).await
     }
 }
+
+
+
+
+#[derive(Debug, Default, Deserialize, Serialize)]
+pub struct Macro {
+}
+impl Macro {
+}
+impl Message for Macro {
+    fn id(&self) -> u8 { 1 }
+}
+#[async_trait]
+pub trait MessageReader { // Sized
+    async fn handle(&self, client: &Client) -> Result<(), Box<dyn Error>>;
+    // fn deserialize(bytes: &[u8]) -> Arc<dyn MessageReader + Sync + Send>;
+}
+#[async_trait]
+pub trait MessageWriter: Message + Serialize {
+    // fn serialize(&self) -> Vec<u8>;
+    fn serialize(&self) -> Vec<u8> {
+        bincode::serialize(&self).unwrap()
+    }
+    async fn send(&self, client: &Client) -> Result<(), Box<dyn Error>> {
+        let mut buf = MessageWriter::serialize(self);
+        buf.insert(0, buf.len() as u8 + 2);
+        buf.insert(1, self.id());
+        client.send_bytes(&buf).await
+    }
+}
+
+
+trait Message {}
+trait MessageHandler<T: Message>  {
+    fn handle(msg: T);
+}
+
+struct ChatMsg {
+    text: String
+}
+impl Message for ChatMsg {}
+
+struct ChatMsgHandler;
+impl<T> MessageHandler<T> for ChatMsgHandler where T : ChatMsg {
+    fn handle(msg: ChatMsg) {
+        println!("received chat message {:?}", msg.text);
+    }
+}
+
+
+#[derive(Debug, Default, Deserialize, Serialize)]
+struct Broad {
+    
+}
+
+impl Broad {
+}
+impl Message for Broad {
+    fn id(&self) -> u8 {
+        1
+    }
+}
+#[derive(Debug, Default, Deserialize, Serialize)]
+struct BroadHandler;
+#[async_trait]
+impl MessageReader for BroadHandler {
+    // fn deserialize(bytes: &[u8]) -> Arc<dyn MessageReader + Sync + Send> {
+    //     let i = bincode::deserialize::<Self>(&bytes[..]).unwrap();
+    //     return Arc::new(i);
+    // }
+    async fn handle(&self, client: &Client) -> Result<(), Box<dyn Error>> {
+        println!("yo we got pong data {:?}", self);
+        Ok(())
+    }
+}
+impl MessageWriter for Broad {}
--- teal/src/net/server.rs
+++ teal/src/net/server.rs
@@ -1,6 +1,6 @@
-
 use std::{error::Error, sync::Arc};
 
+use tokio::sync::broadcast;
 use tokio::{net::TcpListener, sync::Mutex};
 use tracing::{debug, error, info, instrument};
 
@@ -9,55 +9,59 @@ use crate::net::handler::MessageHandlers;
 
 use super::Message;
 
-
 pub struct Server {
-	handlers: Arc<MessageHandlers>,
-	clients: Vec<Arc<Client>>
+    clients: Vec<Arc<Client>>,
 }
 
 impl Server {
-	pub fn new(handlers: Arc<MessageHandlers>) -> Self {
-		Self {
-			handlers,
-			clients: Vec::new()
+
+    pub async fn run(addr: String, handlers: Arc<MessageHandlers>) -> Result<(), Box<dyn Error>> {
+        // Next up we create a TCP listener which will listen for incoming
+        // connections. This TCP listener is bound to the address we determined
+        // above and must be associated with an event loop.
+        let listener = TcpListener::bind(&addr).await?;
+        println!("Listening on: {}", addr);
+
+        let server = Self {
+            clients: Vec::new(),
+        };
+        let server_ptr = Arc::new(Mutex::new(server));
+
+        // let (tx, rx) = broadcast::channel::<&[u8]>(1000);
+        // tokio::select! {
+            
+        // }
+
+        loop {
+            // Asynchronously wait for an inbound socket.
+            let (socket, _addr) = listener.accept().await?;
+
+            // And this is where much of the magic of this server happens. We
+            // crucially want all clients to make progress concurrently, rather than
+            // blocking one on completion of another. To achieve this we use the
+            // `tokio::spawn` function to execute the work in the background.
+            //
+            // Essentially here we're executing a new task to run concurrently,
+            // which will allow all of our clients to be processed concurrently.
+            // let server_ref = Arc::new(self);
+
+            let client: Client = Client::new(socket, handlers.clone(), Some(server_ptr.clone()));
+
+            let client_runner = Arc::new(client);
+            server_ptr.lock().await.clients.push(client_runner.clone());
+
+            tokio::spawn(async move {
+                // client.run().await ;
+                if let Err(err) = client_runner.run().await {
+                    error!(cause = ?err, "client connection error");
+                }
+            });
+        }
+    }
+
+    pub async fn broadcast(&self, msg: Message) {
+		for c in &self.clients {
+			c.send(msg.clone()).await.ok();
 		}
 	}
-	pub async fn run(&mut self, addr: String) -> Result<(), Box<dyn Error>> {
-		// Next up we create a TCP listener which will listen for incoming
-		// connections. This TCP listener is bound to the address we determined
-		// above and must be associated with an event loop.
-		let listener = TcpListener::bind(&addr).await?;
-		println!("Listening on: {}", addr);
-
-		
-		loop {
-			// Asynchronously wait for an inbound socket.
-			let (socket, _) = listener.accept().await?;
-			
-			// And this is where much of the magic of this server happens. We
-			// crucially want all clients to make progress concurrently, rather than
-			// blocking one on completion of another. To achieve this we use the
-			// `tokio::spawn` function to execute the work in the background.
-			//
-			// Essentially here we're executing a new task to run concurrently,
-			// which will allow all of our clients to be processed concurrently.
-
-			let client: Client = Client::new(socket, self.handlers.clone());
-			let client_broadcaster = Arc::new(client);
-			let client_runner = client_broadcaster.clone();
-			self.clients.push(client_broadcaster);
-
-			tokio::spawn(async move {
-				// client.run().await ;
-				if let Err(err) = client_runner.run().await {
-					error!(cause = ?err, "client connection error");
-				}
-			});
-		}
-	}
-
-	pub async fn broadcast(&self, msg: Message) {
-
-	}
-
 }
