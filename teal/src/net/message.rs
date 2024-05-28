a41fa01c94259fece8d4aecd76de34fa92dbf07d
Souchy <robyn.g@hotmail.fr>
2024-05-28 12:53:12 -0400

    Jsp si c'est important, mais met en comment message.rs,. ça compile + ça roule coral/coraline

--- teal/src/net/message.rs
+++ teal/src/net/message.rs
@@ -29,75 +29,75 @@ pub trait MessageScript {
 
 
 
-#[derive(Debug, Default, Deserialize, Serialize)]
-pub struct Macro {
-}
-impl Macro {
-}
-impl Message for Macro {
-    fn id(&self) -> u8 { 1 }
-}
-#[async_trait]
-pub trait MessageReader { // Sized
-    async fn handle(&self, client: &Client) -> Result<(), Box<dyn Error>>;
-    // fn deserialize(bytes: &[u8]) -> Arc<dyn MessageReader + Sync + Send>;
-}
-#[async_trait]
-pub trait MessageWriter: Message + Serialize {
-    // fn serialize(&self) -> Vec<u8>;
-    fn serialize(&self) -> Vec<u8> {
-        bincode::serialize(&self).unwrap()
-    }
-    async fn send(&self, client: &Client) -> Result<(), Box<dyn Error>> {
-        let mut buf = MessageWriter::serialize(self);
-        buf.insert(0, buf.len() as u8 + 2);
-        buf.insert(1, self.id());
-        client.send_bytes(&buf).await
-    }
-}
+// #[derive(Debug, Default, Deserialize, Serialize)]
+// pub struct Macro {
+// }
+// impl Macro {
+// }
+// impl Message for Macro {
+//     fn id(&self) -> u8 { 1 }
+// }
+// #[async_trait]
+// pub trait MessageReader { // Sized
+//     async fn handle(&self, client: &Client) -> Result<(), Box<dyn Error>>;
+//     // fn deserialize(bytes: &[u8]) -> Arc<dyn MessageReader + Sync + Send>;
+// }
+// #[async_trait]
+// pub trait MessageWriter: Message + Serialize {
+//     // fn serialize(&self) -> Vec<u8>;
+//     fn serialize(&self) -> Vec<u8> {
+//         bincode::serialize(&self).unwrap()
+//     }
+//     async fn send(&self, client: &Client) -> Result<(), Box<dyn Error>> {
+//         let mut buf = MessageWriter::serialize(self);
+//         buf.insert(0, buf.len() as u8 + 2);
+//         buf.insert(1, self.id());
+//         client.send_bytes(&buf).await
+//     }
+// }
 
 
-trait Message {}
-trait MessageHandler<T: Message>  {
-    fn handle(msg: T);
-}
+// trait Message {}
+// trait MessageHandler<T: Message>  {
+//     fn handle(msg: T);
+// }
 
-struct ChatMsg {
-    text: String
-}
-impl Message for ChatMsg {}
+// struct ChatMsg {
+//     text: String
+// }
+// impl Message for ChatMsg {}
 
-struct ChatMsgHandler;
-impl<T> MessageHandler<T> for ChatMsgHandler where T : ChatMsg {
-    fn handle(msg: ChatMsg) {
-        println!("received chat message {:?}", msg.text);
-    }
-}
+// struct ChatMsgHandler;
+// impl<T> MessageHandler<T> for ChatMsgHandler where T : ChatMsg {
+//     fn handle(msg: ChatMsg) {
+//         println!("received chat message {:?}", msg.text);
+//     }
+// }
 
 
-#[derive(Debug, Default, Deserialize, Serialize)]
-struct Broad {
+// #[derive(Debug, Default, Deserialize, Serialize)]
+// struct Broad {
     
-}
+// }
 
-impl Broad {
-}
-impl Message for Broad {
-    fn id(&self) -> u8 {
-        1
-    }
-}
-#[derive(Debug, Default, Deserialize, Serialize)]
-struct BroadHandler;
-#[async_trait]
-impl MessageReader for BroadHandler {
-    // fn deserialize(bytes: &[u8]) -> Arc<dyn MessageReader + Sync + Send> {
-    //     let i = bincode::deserialize::<Self>(&bytes[..]).unwrap();
-    //     return Arc::new(i);
-    // }
-    async fn handle(&self, client: &Client) -> Result<(), Box<dyn Error>> {
-        println!("yo we got pong data {:?}", self);
-        Ok(())
-    }
-}
-impl MessageWriter for Broad {}
+// impl Broad {
+// }
+// impl Message for Broad {
+//     // fn id(&self) -> u8 {
+//     //     1
+//     // }
+// }
+// #[derive(Debug, Default, Deserialize, Serialize)]
+// struct BroadHandler;
+// #[async_trait]
+// impl MessageReader for BroadHandler {
+//     // fn deserialize(bytes: &[u8]) -> Arc<dyn MessageReader + Sync + Send> {
+//     //     let i = bincode::deserialize::<Self>(&bytes[..]).unwrap();
+//     //     return Arc::new(i);
+//     // }
+//     async fn handle(&self, client: &Client) -> Result<(), Box<dyn Error>> {
+//         println!("yo we got pong data {:?}", self);
+//         Ok(())
+//     }
+// }
+// impl MessageWriter for Broad {}
